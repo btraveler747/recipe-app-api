ceb5200276cdba5271d0d077c1e8ecab3c89c154
Paul Manning <pmanning@Pauls-MacBook-Pro.local>
2022-06-15 09:27:38 -0500

    Updated requirements

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calcuator Functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
